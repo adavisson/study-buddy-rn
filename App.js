df35965ff88ff10451b46e18867af40121c56c73
Andrew Davisson <akdavisson4@gmail.com>
2020-05-01 08:21:54 -0600

    installed necessary packages

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { CreateAppContainer } from 'react-navigation'
+import { CreateStackNavigator } from 'react-navigation-stack'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -16,4 +18,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
